dbec48ec2b1bf482dfb1d52dd88927c9f17ebbe2
Andrew K <ajkloecker@outlook.com>
2017-10-19 14:07:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = "I don't do much."
 var kittens = ["Milo", "Otis", "Garfield"];
 
 function destructivelyAppendKitten(name){
-  kittens = kittens + name 
+  kittens = [kittens] + [name] 
   return kittens
 }
 function destructivelyPrependKitten(name){
